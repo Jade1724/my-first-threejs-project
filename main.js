13f85195644de3420e347f25ee6d2dedb3311913
Haruka Ichinose <Haruka.Ichinose@umajin.com>
2022-11-30 11:01:48 +1300

    CDN link to threejs library.

--- main.js
+++ main.js
@@ -1,23 +1,4 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from "https://unpkg.com/three@0.126.1/build/three.module.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+const scene = new THREE.Scene();
+console.log(scene);
