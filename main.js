151f8eca4364e474ae3aab6b46568fbbd7f7d348
Haruka Ichinose <Haruka.Ichinose@umajin.com>
2022-11-30 12:26:19 +1300

    Added OrbitConctrol for camera control. Added back light.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from "https://unpkg.com/three@0.126.1/build/three.module.js";
+import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js'
 import * as dat from "dat.gui";
 
 const gui = new dat.GUI();
@@ -47,6 +48,7 @@ renderer.setSize(innerWidth, innerHeight);
 renderer.setPixelRatio(devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
+new OrbitControls(camera, renderer.domElement);
 camera.position.z = 5;
 
 const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
@@ -70,6 +72,10 @@ for (let i = 0; i < array.length; i += 3) {
 const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(0, 0, 1);
 scene.add(light);
+
+const backLight = new THREE.DirectionalLight(0xffffff, 1);
+backLight.position.set(0, 0, -1);
+scene.add(backLight);
 
 function animate() {
     requestAnimationFrame(animate);
