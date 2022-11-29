21c09df911d107c8e83a50e6a8c78eada04d1d5c
Haruka Ichinose <Haruka.Ichinose@umajin.com>
2022-11-30 11:09:06 +1300

    Added a full size canvas and mounted on index.html.

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 import * as THREE from "https://unpkg.com/three@0.126.1/build/three.module.js";
 
 const scene = new THREE.Scene();
-console.log(scene);
+const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(innerWidth, innerHeight)
+document.body.appendChild(renderer.domElement)
