ea382d120e794ffb71a6891b7885d60d1df318c8
Haruka Ichinose <Haruka.Ichinose@umajin.com>
2022-11-30 12:06:17 +1300

    Added dat GUI that modulate the width of the plane.

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
 import * as THREE from "https://unpkg.com/three@0.126.1/build/three.module.js";
+import * as dat from "dat.gui";
+
+const gui = new dat.GUI();
+const world = {
+    plane: {
+        width: 10,
+    },
+};
+gui.add(world.plane, "width", 1, 20).onChange(() => {
+    planeMesh.geometry.dispose();
+    planeMesh.geometry = new THREE.PlaneGeometry(world.plane.width, 5, 10, 10);
+
+    const { array } = planeMesh.geometry.attributes.position;
+    for (let i = 0; i < array.length; i += 3) {
+        const x = array[i];
+        const y = array[i + 1];
+        const z = array[i + 2];
+
+        array[i + 2] = z + Math.random();
+    }
+});
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -19,21 +40,21 @@ const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
 const planeMaterial = new THREE.MeshPhongMaterial({
     color: 0xff0000,
     side: THREE.DoubleSide,
-    flatShading: THREE.FlatShading
+    flatShading: THREE.FlatShading,
 });
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(planeMesh);
 
 const { array } = planeMesh.geometry.attributes.position;
 for (let i = 0; i < array.length; i += 3) {
-  const x = array[i]
-  const y = array[i+1];
-  const z = array[i+2];
+    const x = array[i];
+    const y = array[i + 1];
+    const z = array[i + 2];
 
-  array[i + 2] = z + Math.random()
+    array[i + 2] = z + Math.random();
 }
 
-const light = new THREE.DirectionalLight(0xFFFFFF, 1)
+const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(0, 0, 1);
 scene.add(light);
 
