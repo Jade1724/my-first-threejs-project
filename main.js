d16def7ca9708f2dcb6545e39288637ce54c5eef
Haruka Ichinose <Haruka.Ichinose@umajin.com>
2022-11-30 11:43:44 +1300

    Modified z position of plane vertices randomely.

--- main.js
+++ main.js
@@ -19,9 +19,19 @@ const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
 const planeMaterial = new THREE.MeshPhongMaterial({
     color: 0xff0000,
     side: THREE.DoubleSide,
+    flatShading: THREE.FlatShading
 });
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(planeMesh);
+
+const { array } = planeMesh.geometry.attributes.position;
+for (let i = 0; i < array.length; i += 3) {
+  const x = array[i]
+  const y = array[i+1];
+  const z = array[i+2];
+
+  array[i + 2] = z + Math.random()
+}
 
 const light = new THREE.DirectionalLight(0xFFFFFF, 1)
 light.position.set(0, 0, 1);
